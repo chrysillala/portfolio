1d3982c5bce6a450ef45682bc973f0ca118a4f09
chrysilla mayasari <chrysilla_mysr@hotmail.com>
2020-06-03 22:56:16 +0700

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 // import './registerServiceWorker'
-
+import VueGtag from "vue-gtag";
 import '@/assets/css/tailwind.css'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: "UA-168469791-1" }
+});
 
 new Vue({
   render: h => h(App),
